bddf46b5fe49b7bac0ea573cefb909c953953941
dtpvy <dtpy0202@gmail.com>
2021-09-19 00:40:58 +0700

    update order details

--- screens/OrderScreens/OrderDetails.js
+++ screens/OrderScreens/OrderDetails.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { FlatList, View, Text, StyleSheet, ScrollView } from 'react-native';
 import ViewProductOrder from '../../components/Products/ViewProductOrder';
 
 const currentLocation = "http://192.168.1.103:3000/api";
@@ -23,28 +23,51 @@ export default function OrderDetails({ route }) {
   }, []);
 
   return (
-    <View>
-      <View>
-        <Text>#{order.id}</Text>
-      </View>
-      <View>
-        <Text>Vận chuyển</Text>
-        <Text>{order.status}</Text>
-      </View>
-      <View>
-        <Text>Sản phẩm</Text>
-        <FlatList
-          data={order.products}
-          renderItem={({ item }) =>
-            <ViewProductOrder product={item} />
-          }
-          keyExtractor={item => `${item.id}`}
-        />
-      </View>
-      <View>
-        <Text>Thành tiền:</Text>
-        <Text>{total}</Text>
-      </View>
+    <View style={styles.container}>
+      <ScrollView>
+        <View>
+          <Text style={styles.title}>Thông tin khách hàng</Text>
+          <Text>Họ và tên: {order.user.name}</Text>
+          <Text>Số điện thoại: {order.user.number}</Text>
+          <Text>Địa chỉ: {order.user.address}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.title}>Trạng thái đơn hàng: </Text>
+          <Text>{order.status}</Text>
+        </View>
+        <View>
+          <Text style={styles.title}>Sản phẩm</Text>
+          <FlatList
+            data={order.products}
+            renderItem={({ item }) =>
+              <ViewProductOrder product={item} />
+            }
+            keyExtractor={item => `${item.id}`}
+          />
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.title}>Thành tiền: </Text>
+          <Text>{total}</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 16,
+    fontSize: 13
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  row: {
+    flexDirection: 'row',
+    marginVertical: 8,
+    alignItems: 'flex-end'
+  }
+});
